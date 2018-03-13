7ae1c707a57eb540f1b0f76364c9327ccadb071a
Harshit kyal <harshitkyal@gmail.com>
2018-03-13 23:52:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,10 +72,6 @@ app.use( bodyParser.urlencoded({
 app.use( session({ 
 	secret: 'cookie_secret',
 	name:   'kaas',
-	store:  new RedisStore({
-		host: '127.0.0.1',
-		port: 6379
-	}),
 	proxy:  true,
     resave: true,
     saveUninitialized: true
